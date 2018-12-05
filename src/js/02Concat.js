8285c5191886aab5ab31f92d27415b33762d0564
Sergio Martín <sermmor@outlook.com>
2018-12-05 11:29:14 +0100

    Concat finished

--- src/js/02Concat.js
+++ src/js/02Concat.js
@@ -1,4 +1,7 @@
+const concat = (a, b) => [...a, ...b];
 
-// Show test in console.
+// -------- Show tests in console.
 console.log("-- 2. Concat --");
-
+let list1 = [20, 42, 61, 57];
+let list2 = [64, 98, 4];
+console.log("Concat result of [20, 42, 61, 57] + [64, 98, 4]: " + concat(list1, list2));
